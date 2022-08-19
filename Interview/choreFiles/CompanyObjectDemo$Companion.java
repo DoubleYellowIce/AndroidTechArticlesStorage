32d4010b7d0055b25f8b9144e74cca01d61e83fd
hbhuang <hbhuang01@gmail.com>
2022-08-19 15:45:33 +0800

    docs: 增加companion object的问答

---
+++ Interview/choreFiles/CompanyObjectDemo$Companion.java
@@ -0,0 +1,13 @@
+import kotlin.jvm.internal.DefaultConstructorMarker;
+import kotlin.Metadata;
+
+// 
+// Decompiled by Procyon v0.5.36
+// 
+
+@Metadata(mv = { 1, 6, 0 }, k = 1, xi = 48, d1 = { "\u0000\f\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\b\u0086\u0003\u0018\u00002\u00020\u0001B\u0007\b\u0002¢\u0006\u0002\u0010\u0002¨\u0006\u0003" }, d2 = { "LCompanyObjectDemo$Companion;", "", "()V", "Leetcode" })
+public static final class Companion
+{
+    private Companion() {
+    }
+}
